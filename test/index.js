10f0d8bcadbc0caeee4276b8ca39e9fd15daf243
Dandapani, Aravindan <aravindmicromax@gmail.com>
2019-08-07 17:36:16 -0600

    mocha and istanbul integration

--- test/index.js
+++ test/index.js
@@ -1,14 +1,78 @@
-import http from 'http';
-import assert from 'assert';
+import { expect } from "chai";
+import request from "supertest";
+import server from "../lib/server";
 
-import server from '../lib/index.js';
+describe("# Get all Products", function() {
+  it("should get all products", function(done) {
+    request(server)
+      .get("/api/products/")
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
+  it("should get product for the id 5d432ed86498bae8c0d2d953", function(done) {
+    request(server)
+      .get("/api/products/5d432ed86498bae8c0d2d953")
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
 
-describe('Example Node Server', () => {
-  it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
-      assert.equal(200, res.statusCode);
-      server.close();
-      done();
-    });
+  it("should get 404 for the id 5d432ed86498bae8c0d2d959", function(done) {
+    request(server)
+      .get("/api/products/5d432ed86498bae8c0d2d959")
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+  });
+
+  it("should get 500 for the invalud Objectid 5d4b59c4565b5b4726ed63bh", function(done) {
+    request(server)
+      .get("/api/products/5d4b59c4565b5b4726ed63bh")
+      .end(function(err, res) {
+        expect(res.statusCode).to.equal(500);
+        done();
+      });
+  });
+});
+
+describe("Create a Product", function() {
+  it("Should get 201 upon successful creation of product", function(done) {
+    request(server)
+      .post("/api/products/")
+      .send({
+        name: "Pixel - Beta",
+        quantity: 45,
+        available: true,
+
+        metadata: {
+          imported: false,
+          description: "Google Pixel"
+        }
+      })
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(201)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("Should get 500 when the payload not matching with product schema defined by mongoose", function(done) {
+    request(server)
+      .post("/api/products/")
+      .send({
+        age: 1
+      })
+      .set("Accept", "application/json")
+      .expect(500)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
   });
 });
